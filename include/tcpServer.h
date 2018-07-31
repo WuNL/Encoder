8b4de56210dd40267a9857bb2b62535585c68679
WuNL <wunailiang@gmail.com>
2018-07-31 14:16:39 +0800

    first commit set cmake

---
+++ include/tcpServer.h
@@ -0,0 +1,15 @@
+//
+// Created by sdt on 18-7-31.
+//
+
+#ifndef ENCODER_TCPSERVER_H
+#define ENCODER_TCPSERVER_H
+
+
+class tcpServer
+{
+
+};
+
+
+#endif //ENCODER_TCPSERVER_H
---
+++ src/tcpServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sdt on 18-7-31.
+//
+
+#include "../include/tcpServer.h"
